cf5b60f802da6a1a0b3813aa332ba668e9795641
Mariam Eissa <58251849+mariamkhaled99@users.noreply.github.com>
2023-06-20 21:59:00 +0300

    reset password dj_Rest_auth test

--- core/settings.py
+++ core/settings.py
@@ -289,7 +289,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-'
+
 # EMAIL_HOST_PASSWORD = "vpvsejvtrnfhnzgy"
 # os.environ.get('EMAIL_HOST_PASSWORD')
 # for image files

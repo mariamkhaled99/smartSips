82b35c3bb6febe52c376fccbafa6178c11546dbb
Mariam Eissa <58251849+mariamkhaled99@users.noreply.github.com>
2023-06-21 01:54:22 +0300

    reset password dj_Rest_auth test try 3

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-8!zahxpz3!276sqqcrg)9xpes-@td(^6w*5zu(seyltxfdtlls'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -289,7 +290,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = "mariam.e20171@gmail.com"
 EMAIL_HOST_PASSWORD = 'yfuelymznrxszazv'
 # os.environ.get('EMAIL_HOST_USER')

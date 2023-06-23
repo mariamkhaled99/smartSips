99533d3ae63b47430a0a7aa2eed7fb70c3cdfb46
Mariam Eissa <58251849+mariamkhaled99@users.noreply.github.com>
2023-06-23 06:33:30 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -9,22 +9,22 @@ import random
 
 def iot_client():
     filename = 'IrrigationWater_Final_Dataset.csv'
-    ser = serial.Serial('com6',9600)
-    time.sleep(1)
-    while True:
-        while (ser.in_waiting==0):
-            pass
-        with open(filename, 'r') as f:
-            reader = csv.reader(f)
-            rows = list(reader)
-        random_row = random.choice(rows)
-        line = ser.readline()
-        data = str(line,'utf-8')
-        data=data.strip('\r\n')
-        data = data.split(',')
+    # ser = serial.Serial('com6',9600)
+    # time.sleep(1)
+    # while True:
+    #     while (ser.in_waiting==0):
+    #         pass
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+    random_row = random.choice(rows)
+        # line = ser.readline()
+        # data = str(line,'utf-8')
+        # data=data.strip('\r\n')
+        # data = data.split(',')
         data=random_row
-        print(data )
-        return data
+    print(data )
+    return data
         
 msg=iot_client()
 url = 'https://smartsips-production.up.railway.app/publish_message'

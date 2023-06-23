6ab63d400e48015d8a85a011496209eef10d54a2
Mariam Eissa <58251849+mariamkhaled99@users.noreply.github.com>
2023-06-23 06:41:22 +0300

    iot api4

--- core/views.py
+++ core/views.py
@@ -9,26 +9,26 @@ import random
 
 def iot_client():
     filename = 'IrrigationWater_Final_Dataset.csv'
-    ser = serial.Serial('com6',9600)
-    time.sleep(1)
-    while True:
-        while (ser.in_waiting==0):
-            pass
-        with open(filename, 'r') as f:
-            reader = csv.reader(f)
-            rows = list(reader)
-        random_row = random.choice(rows)
-        line = ser.readline()
-        data = str(line,'utf-8')
-        data=data.strip('\r\n')
-        data = data.split(',')
-        data=random_row
-        print(data )
-        return data
+    # ser = serial.Serial('com6',9600)
+    # time.sleep(1)
+    # while True:
+    #     while (ser.in_waiting==0):
+    #         pass
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+    random_row = random.choice(rows)
+    # line = ser.readline()
+    # data = str(line,'utf-8')
+    # data=data.strip('\r\n')
+    # data = data.split(',')
+    data=random_row
+    print(data )
+    return data
         
 msg=iot_client()
 url = 'https://smartsips-production.up.railway.app/publish_message'
-data = {'topic': 'django/mqtt', 'msg': 'msg'}
+data = {'topic': 'django/mqtt', 'msg': msg}
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, data=json.dumps(data), headers=headers)
     
